080d396746a7458eaa260b498323d44a47bb5952
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-08-06 16:48:47 +0900

    slot

--- 03-cli/vuejs/src/App.vue
+++ 03-cli/vuejs/src/App.vue
@@ -1,7 +1,30 @@
 <template>
   <div>
-    <LikeHeader></LikeHeader>
-    <h2>{{number}}</h2>
+    <LikeHeader>
+      <!-- 名前付きslot -->
+      <!-- #で省略できる -->
+      <!-- <template v-slot:[title]> -->
+      <template #title>
+        <h1>トータルのいいね数</h1>
+      </template>
+      <!-- <template v-slot:number> -->
+      <template #number>
+        <h2>{{number}}</h2>
+      </template>
+      <!-- デフォルトslot -->
+      <p>こんにちは</p>
+      <!-- 暗黙的に下記のようになる -->
+      <!--
+      <template v-slot:default>
+        <p>こんにちは</p>
+      </template>
+      -->
+      <template v-slot:user="slotProps">
+        <p>{{slotProps}}</p>
+        <p>{{slotProps.user.name}}</p>
+      </template>
+    </LikeHeader>
+
     <!-- <LikeNumber :totalNumber="number" v-on:my-click="number = $event"></LikeNumber> -->
     <LikeNumber :totalNumber="number" v-on:my-click="incrementNumber"></LikeNumber>
     <LikeNumber :total-number="number"></LikeNumber>
@@ -15,7 +38,8 @@ import LikeHeader from './components/LikeHeader.vue';
 export default {
   data() {
     return {
-      number: 10
+      number: 10,
+      title: "title"
     }
   },
   methods: {
--- 03-cli/vuejs/src/components/LikeHeader.vue
+++ 03-cli/vuejs/src/components/LikeHeader.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>いいね</h1>
+  <div>
+    <!-- 親から何も渡って来なければ、デフォルトが表示される -->
+    <slot name="title">デフォルト</slot>
+    <slot></slot>
+    <slot name="number"></slot>
+
+    <!-- スロットプロパティ 子から親にデータを上げる -->
+    <slot name="user" :user="user" text="text"></slot>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "hoge"
+      }
+    }
+  },
+
+}
+</script>
