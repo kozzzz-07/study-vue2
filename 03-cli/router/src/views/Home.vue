5cde2b2c3e952e382b767b4ed452bd40c7f11ed6
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-08-11 16:24:55 +0900

    vuex

--- 03-cli/router/src/views/Home.vue
+++ 03-cli/router/src/views/Home.vue
@@ -2,18 +2,38 @@
   <div>
     <h3>Home</h3>
     <button @click="toUsers">Go Users</button>
+    <p>{{ count }}</p>
+    <p>{{ myDoubleCount }}</p>
   </div>
 </template>
 
 <script>
+// gettersを取得できる
+import { mapGetters } from "vuex";
+
 export default {
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+    // doubleCount() {
+    //   return this.$store.getters.doubleCount;
+    // }
+    ...mapGetters(["doubleCount"]),
+    // 別名もつけられる
+    // ...mapGetters({myDoubleCount: "doubleCount"}),
+  },
   methods: {
     toUsers() {
       // this.$router.push('users')
       // this.$router.push({path: 'users'})
 
       // paramsはnameのみにしか使えない
-      this.$router.push({ name: "user-id-profile", params: { id: 1 }, hash: '#next-user'});
+      this.$router.push({
+        name: "user-id-profile",
+        params: { id: 1 },
+        hash: "#next-user",
+      });
     },
   },
 };
