fc467011d726558bd2adcf557f53d35514e94308
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-08-11 17:28:36 +0900

    actions

--- 03-cli/router/src/store.js
+++ 03-cli/router/src/store.js
@@ -5,19 +5,28 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    count: 2
+    count: 2,
   },
   getters: {
-    doubleCount: state => state.count * 2
+    doubleCount: (state) => state.count * 2,
   },
   // mutationsでのみstateを更新すること
   // 同期的な処理しか書けない
   mutations: {
     increment(state, number) {
-      state.count += number
+      state.count += number;
     },
     decrement(state, number) {
-      state.count -= number
+      state.count -= number;
     },
-  }
+  },
+  // actionsからのみcommitを使うという方針でも良い
+  actions: {
+    increment({ commit }, number) {
+      commit("increment", number);
+    },
+    decrement({ commit }, number) {
+      commit("decrement", number);
+    },
+  },
 });
--- 03-cli/router/src/views/HeaderHome.vue
+++ 03-cli/router/src/views/HeaderHome.vue
@@ -10,24 +10,41 @@
         >Users</router-link
       >
     </nav>
+
+    <!-- mapActions、mapMutationsではcommitの第二引数を関数の引数として渡して同じ意味になる -->
     <button @click="increment(2)">+1</button>
     <button @click="decrement(2)">-1</button>
+
+    <!-- <button @click="increment">+1</button>
+    <button @click="decrement">-1</button> -->
   </div>
 </template>
 
 <script>
-import {mapMutations} from 'vuex'
+// import {mapMutations} from 'vuex'
+import { mapActions } from "vuex";
 export default {
   methods: {
-    ...mapMutations(["increment", "decrement"]),
+    ...mapActions(["increment", "decrement"])
     // increment() {
+    //   // dispatchでactionsを発火
+    //   this.$store.dispatch("increment", 2);
+    // },
+    // decrement() {
+    //   // dispatchでactionsを発火
+    //   this.$store.dispatch("decrement", 2);
+    // },
+
+    // ...mapMutations(["increment", "decrement"]),
+    // increment() {
+    //   // commitでmutationsを発火
     //   this.$store.commit("increment", 2);
     // },
     // decrement() {
     //   this.$store.commit("decrement", 2);
     // }
   },
-}
+};
 </script>
 
 <style scoped>
